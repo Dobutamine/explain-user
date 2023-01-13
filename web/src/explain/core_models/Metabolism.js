e81aca5a72c17928d99b46bfa50ebde37ff2cfc2
dobutamine <tim.antonius@outlook.com>
2023-01-14 00:30:43 +0100

    added metabolism

--- web/src/explain/core_models/Metabolism.js
+++ web/src/explain/core_models/Metabolism.js
@@ -1,3 +1,46 @@
 import ModelBaseClass from "../helpers/ModelBaseClass";
 
-export class Metabolism extends ModelBaseClass {}
+export class Metabolism extends ModelBaseClass {
+  CalcModel() {
+    // translate the VO2 in ml/kg/min to VO2 in mmol for this stepsize (assumption is 37 degrees and atmospheric pressure)
+    let vo2_step =
+      ((0.039 * this.Vo2 * this._modelEngine.Weight) / 60.0) * this._t;
+
+    // do the metabolism for each active blood compliance
+    Object.entries(this.MetabolicActiveModels).forEach(([met_model, fvo2]) => {
+      // get the to2 from the blood compartment
+      let to2 = this._modelEngine.Models[met_model].To2;
+
+      // calculate the change in oxygen concentration in this step
+      let dto2 = vo2_step * fvo2;
+
+      // calculate the new oxygen concentration in blood
+      let new_to2 =
+        (to2 * this._modelEngine.Models[met_model].Vol - dto2) /
+        this._modelEngine.Models[met_model].Vol;
+      // guard against negative values
+      if (new_to2 < 0) {
+        new_to2 = 0;
+      }
+
+      // get the tco2 from the blood compartment
+      let tco2 = this._modelEngine.Models[met_model].Tco2;
+
+      // calculate the change in co2 concentration in this step
+      let dtco2 = vo2_step * fvo2 * this.RespQ;
+
+      // calculate the new oxygen concentration in blood
+      let new_tco2 =
+        (tco2 * this._modelEngine.Models[met_model].Vol + dtco2) /
+        this._modelEngine.Models[met_model].Vol;
+      // guard against negative values
+      if (new_tco2 < 0) {
+        new_tco2 = 0;
+      }
+
+      // store the new to2 and tco2
+      this._modelEngine.Models[met_model].To2 = new_to2;
+      this._modelEngine.Models[met_model].Tco2 = new_tco2;
+    });
+  }
+}
