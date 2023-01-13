477bbf22e5e28ffaf4cac7d689c49aa060ea4346
dobutamine <tim.antonius@outlook.com>
2023-01-13 20:26:32 +0100

    acidbase and oxygenation

--- web/src/explain/core_models/AcidBase.js
+++ web/src/explain/core_models/AcidBase.js
@@ -1,3 +1,74 @@
 import { Brent } from "../helpers/BrentRootFinding";
 
-export default class AcidBase {}
+export default class AcidBase {
+  // class attributes
+  AlphaCo2P = 0.03067;
+
+  // set the brent root finding properties
+  _brent_accuracy = 1e-8;
+  _max_iterations = 100;
+  _left_hp = Math.pow(10.0, -7.8) * 1000.0;
+  _right_hp = Math.pow(10.0, -6.8) * 1000.0;
+
+  // set acid base constants
+  _kw = Math.pow(10.0, -13.6) * 1000.0;
+  _kc = Math.pow(10.0, -6.1) * 1000.0;
+  _kd = Math.pow(10.0, -10.22) * 1000.0;
+
+  // blood gas
+  _tco2 = 0;
+  _sid = 0;
+  _albumin = 0;
+  _phosphates = 0;
+  _uma = 0;
+  _ph = 0;
+  _pco2 = 0;
+  _hco3 = 0;
+  _cco2 = 0;
+  _cco3 = 0;
+  _oh = 0;
+
+  calc_acid_base(tco2, sid = 35.9, alb = 25.0, pi = 1.64, u = 0.0) {
+    // declare a new blood gas instance
+    let _result = new BloodGas();
+
+    // store the parameters
+    this._tco2 = tco2;
+    this._sid = sid;
+    this._phosphates = pi;
+    this._albumin = alb;
+    this._uma = u;
+
+    // find the hp concentration
+    let r = Brent(
+      this.net_charge_plasma,
+      this._left_hp,
+      this._right_hp,
+      this._max_iterations,
+      this._brent_accuracy
+    );
+    _result.Iterations = r[1];
+    _result.Error = r[2];
+
+    // if the brent root finding did not yield a value then return an error
+    if (_result.Error) {
+      return _result;
+    }
+
+    // complete the result object and return it
+    _result.Ph = this._ph;
+    _result.Pco2 = this._pco2;
+    _result.Hco3 = this._hco3;
+
+    return _result;
+  }
+}
+
+class BloodGas {
+  Ph = 0;
+  Pco2 = 0;
+  Hco3 = 0;
+  Be = 0;
+  Iterations = 0;
+  Error = true;
+}
--- web/src/explain/core_models/Oxygenation.js
+++ web/src/explain/core_models/Oxygenation.js
@@ -1,3 +1,124 @@
 import { Brent } from "../helpers/BrentRootFinding";
 
-export default class Oxygenation {}
+export default class Oxygenation {
+  // class attributes
+  Hemoglobin = 0.0;
+  Dpg = 0.0;
+
+  // set the brent root finding properties
+  _brent_accuracy = 1e-8;
+  _max_iterations = 100;
+  _left_o2 = 0.01;
+  _right_o2 = 100.0;
+
+  // set constants
+  _mmol_to_ml = 22.2674;
+  _gas_constant = 62.36367;
+
+  // independent parameters
+  _to2 = 0;
+  _ph = 0;
+  _temp = 0;
+  _be = 0;
+
+  // dependent parameters
+  _so2 = 0;
+  _po2 = 0;
+  _pres = 760;
+
+  calc_oxygenation(
+    to2,
+    hb = 8.0,
+    temp = 37.0,
+    ph = 7.4,
+    dpg = 5,
+    be = 0.0,
+    pres = 760
+  ) {
+    // define a result object
+    let _result = new OxyResult();
+
+    // import the parameters
+    this._to2 = to2;
+    this._hemoglobin = hb;
+    this._temp = temp;
+    this._ph = ph;
+    this._dpg = dpg;
+    this._be = be;
+    this._pres = pres;
+
+    // find the po2 and so2 and using a brent root finding procedure
+
+    // the brent root finding returns a tuple (result: float, iterations: float, error: bool)
+    let r = Brent(
+      this.oxygen_content,
+      this._left_o2,
+      this._right_o2,
+      this._max_iterations,
+      this._brent_accuracy
+    );
+    _result.Iterations = r[1];
+    _result.Error = r[2];
+
+    // if the brent root finding did not yield a value then return an error
+    if (_result.Error) {
+      return _result;
+    }
+
+    // complete the result object and return it
+    _result.Po2 = this._po2;
+    _result.So2 = this._so2;
+
+    return _result;
+  }
+
+  oxygen_content(po2) {
+    // calculate the saturation from the current po2 from the current po2 estimate
+    this._so2 = this.oxygen_dissociation_curve(po2);
+
+    // store the current po2 estimate as mmHg
+    this._po2 = po2 / 0.13333;
+
+    // calculate the to2 from the current po2 estimate
+    // convert the hemoglobin unit from mmol/l to g/dL
+    // convert the po2 from kPa to mmHg
+    // convert to output from ml O2/dL blood to ml O2/l blood
+    let to2_new_estimate =
+      (0.0031 * (po2 / 0.1333) +
+        1.36 * (this._hemoglobin / 0.6206) * this._so2) *
+      10.0;
+
+    // convert the ml O2/l to mmol/l with the gas law with (GasConstant * (273.15 + _temp)) / Pres) / to2 (mol/l)
+    let _mmolToMl = (this._gas_constant * (273.15 + this._temp)) / this._pres;
+
+    // calculate ml O2 / ml blood.
+    to2_new_estimate = to2_new_estimate / this._mmol_to_ml;
+
+    // calculate the difference between the real to2 and the to2 based on the new po2 estimate and return it to the brent root finding function
+    let dto2 = this._to2 - to2_new_estimate;
+
+    // return the difference
+    return dto2;
+  }
+
+  oxygen_dissociation_curve(po2) {
+    // calculate the saturation from the po2 depending on the ph, be, temperature and dpg level.
+    let a =
+      1.04 * (7.4 - this._ph) + 0.005 * this._be + 0.07 * (this._dpg - 5.0);
+    let b = 0.055 * (this._temp + 273.15 - 310.15);
+    let y0 = 1.875;
+    let x0 = 1.875 + a + b;
+    let h0 = 3.5 + a;
+    let k = 0.5343;
+    let x = Math.log(po2, Math.E);
+    let y = x - x0 + h0 * Math.tanh(k * (x - x0)) + y0;
+    return 1.0 / (Math.pow(math.e, -y) + 1.0);
+  }
+}
+
+class OxyResult {
+  Po2 = 0;
+  So2 = 0;
+  Error = true;
+  Iterations = 0;
+}
